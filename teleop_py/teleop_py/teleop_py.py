1ab6575861fb216c539cb9e155c895ee793f8e94
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2025-11-08 10:29:42 +0700

    teleop_py xịn (topic /cmdvel1)

--- teleop_py/teleop_py/teleop_py.py
+++ teleop_py/teleop_py/teleop_py.py
@@ -1,16 +1,53 @@
-#!/usr/bin/env python3
-import sys
-import select
-import termios
-import tty
+# Copyright 2011 Brown University Robotics.
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
+import sys
+import threading
+
+import geometry_msgs.msg
+import rcl_interfaces.msg
 import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
+
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import termios
+    import tty
 
 
 msg = """
-Reading from the keyboard and Publishing to Twist!
+This node takes keypresses from the keyboard and publishes them
+as Twist/TwistStamped messages. It works best with a US keyboard layout.
 ---------------------------
 Moving around:
    u    i    o
@@ -58,87 +95,140 @@ moveBindings = {
 
 speedBindings = {
     'q': (1.1, 1.1),
-    'z': (0.9, 0.9),
-    'w': (1.1, 1.0),
-    'x': (0.9, 1.0),
-    'e': (1.0, 1.1),
-    'c': (1.0, 0.9),
+    'z': (.9, .9),
+    'w': (1.1, 1),
+    'x': (.9, 1),
+    'e': (1, 1.1),
+    'c': (1, .9),
 }
 
 
-def get_key(settings):
-    """Đọc một phím từ bàn phím mà không cần nhấn Enter"""
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], 0)
-    key = sys.stdin.read(1) if rlist else ''
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+def getKey(settings):
+    if sys.platform == 'win32':
+        # getwch() returns a string on Windows
+        key = msvcrt.getwch()
+    else:
+        tty.setraw(sys.stdin.fileno())
+        # sys.stdin.read() returns a string on Linux
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
 
+def saveTerminalSettings():
+    if sys.platform == 'win32':
+        return None
+    return termios.tcgetattr(sys.stdin)
+
+
+def restoreTerminalSettings(old_settings):
+    if sys.platform == 'win32':
+        return
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+
 def vels(speed, turn):
-    return f"currently:\tspeed {speed:.2f}\tturn {turn:.2f}"
+    return 'currently:\tspeed %.2f\tturn %.2f ' % (speed, turn)
 
 
-class TeleopTwistKeyboard(Node):
-    def __init__(self):
-        super().__init__('teleop_twist_keyboard')
-        self.pub = self.create_publisher(Twist, 'cmd_vel1', 10)
+def main():
+    settings = saveTerminalSettings()
 
-        self.speed = 0.5
-        self.turn = 1.0
-        self.x = self.y = self.z = self.th = 0.0
-        self.status = 0
+    rclpy.init()
 
-    def run(self):
-        settings = termios.tcgetattr(sys.stdin)
+    node = rclpy.create_node('teleop_twist_keyboard')
 
-        try:
-            print(msg)
-            print(vels(self.speed, self.turn))
+    # parameters
+    read_only_descriptor = rcl_interfaces.msg.ParameterDescriptor(read_only=True)
+    stamped = node.declare_parameter('stamped', False, read_only_descriptor).value
+    frame_id = node.declare_parameter('frame_id', '', read_only_descriptor).value
+    speed = node.declare_parameter('speed', 0.5, read_only_descriptor).value
+    turn = node.declare_parameter('turn', 1.0, read_only_descriptor).value
 
-            while True:
-                key = get_key(settings)
+    if not stamped and frame_id:
+        raise Exception("'frame_id' can only be set when 'stamped' is True")
 
-                if key in moveBindings:
-                    self.x, self.y, self.z, self.th = moveBindings[key]
-                elif key in speedBindings:
-                    self.speed *= speedBindings[key][0]
-                    self.turn *= speedBindings[key][1]
+    if stamped:
+        TwistMsg = geometry_msgs.msg.TwistStamped
+    else:
+        TwistMsg = geometry_msgs.msg.Twist
 
-                    print(vels(self.speed, self.turn))
-                    if self.status == 14:
-                        print(msg)
-                    self.status = (self.status + 1) % 15
-                else:
-                    self.x = self.y = self.z = self.th = 0.0
-                    if key == '\x03':  # Ctrl+C
-                        break
+    pub = node.create_publisher(TwistMsg, 'cmd_vel1', 10)
 
-                twist = Twist()
-                twist.linear.x = self.x * self.speed
-                twist.linear.y = self.y * self.speed
-                twist.linear.z = self.z * self.speed
-                twist.angular.z = self.th * self.turn
-                self.pub.publish(twist)
+    spinner = threading.Thread(target=rclpy.spin, args=(node,))
+    spinner.start()
 
-        except Exception as e:
-            print(f"Error: {e}")
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    th = 0.0
+    status = 0.0
 
-        finally:
-            twist = Twist()  # Dừng robot khi thoát
-            self.pub.publish(twist)
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-            print("\nTeleop terminated.")
+    twist_msg = TwistMsg()
 
+    if stamped:
+        twist = twist_msg.twist
+        twist_msg.header.stamp = node.get_clock().now().to_msg()
+        twist_msg.header.frame_id = frame_id
+    else:
+        twist = twist_msg
 
-def main(args=None):
-    rclpy.init(args=args)
-    teleop = TeleopTwistKeyboard()
-    teleop.run()
-    teleop.destroy_node()
-    rclpy.shutdown()
+    try:
+        print(msg)
+        print(vels(speed, turn))
+        while True:
+            key = getKey(settings)
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
+
+                print(vels(speed, turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+            else:
+                x = 0.0
+                y = 0.0
+                z = 0.0
+                th = 0.0
+                if (key == '\x03'):
+                    break
+
+            if stamped:
+                twist_msg.header.stamp = node.get_clock().now().to_msg()
+
+            twist.linear.x = x * speed
+            twist.linear.y = y * speed
+            twist.linear.z = z * speed
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = th * turn
+            pub.publish(twist_msg)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if stamped:
+            twist_msg.header.stamp = node.get_clock().now().to_msg()
+
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+        pub.publish(twist_msg)
+        rclpy.shutdown()
+        spinner.join()
+
+        restoreTerminalSettings(settings)
 
 
 if __name__ == '__main__':
     main()
-
